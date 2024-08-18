ea350fc54ed91919f9d2301d360e45c3c5cbd87e
Swayam Soni <80837512+string-SAm@users.noreply.github.com>
2024-08-19 01:01:23 +0530

    Add files via upload

---
+++ components/Navbar.jsx
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+import { useSelector } from 'react-redux';
+import styles from '../styles/Navbar.module.css';
+
+const Navbar = () => {
+  const cart = useSelector((state) => state.cart);
+
+  const getItemsCount = () => {
+    return cart.reduce((accumulator, item) => accumulator + item.quantity, 0);
+  };
+
+
+  return (
+    <nav className={styles.navbar}>
+      <h6 className={`${styles.logo} ${styles.navlink}`}> <Link href="/">ShoppingKart</Link></h6>
+      <ul className={styles.links}>
+        <li className={styles.navlink}>
+          <Link href="/">Home</Link>
+        </li>
+        <li className={styles.navlink}>
+          <Link href="/shop">Shop</Link>
+        </li>
+        <li className={styles.navlink}>
+          <Link href="/cart">
+            <a>Cart ({getItemsCount()})</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
